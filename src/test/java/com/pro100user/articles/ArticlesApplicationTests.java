c4b4bf6983cb61bf3fe9503a10cbf7fef1117082
pro100user <bt769271@gmail.com>
2022-07-09 01:19:12 +0300

    init

---
+++ src/test/java/com/pro100user/articles/ArticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro100user.articles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
